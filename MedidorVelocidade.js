3b4387b991324bcc16267c1fd2a400b86f0343d3
emanuel-passos <emanuelpassosdoa@hotmail.com>
2022-02-21 17:08:14 -0300

    medidor de velocidade

---
+++ MedidorVelocidade.js
@@ -0,0 +1,17 @@
+
+console.log(verificaVelocidade(84));
+
+function verificaVelocidade(velocidade){
+
+    if (velocidade <= 70){
+        return "ok"
+    }else{
+        const pontos = Math.floor(((velocidade - 70) / 5)) ;
+        if(pontos >= 12)
+            console.log("carteira suspensa")
+        else
+            console.log("pontos: ", pontos)
+        
+       
+    }
+}
